478af2eb50f40164081203c5f35e2da4c6909401
admin <admin@admin-PC>
2018-08-26 17:00:08 +0530

    Commit2

---
+++ SelDemo1Mav/src/test/java/library/Constants.java
@@ -0,0 +1,9 @@
+package library;
+
+public interface Constants {
+	
+	public String CHROME_PATH="./driver/chromedriver.exe";
+	public String CHROME_DRIVER="webdriver.chrome.driver";
+	public 	String TEST_URL="https://google.com";
+
+}
